755c8227e497410fa2fcacc9b107c9b225a70735
Ahmed Shibani <a.shibani@email.ly>
2020-12-12 15:23:57 +0200

    Added config.ini.example and requirements.txt

--- zbx-to-cachet.py
+++ zbx-to-cachet.py
@@ -1,9 +1,11 @@
+#!/usr/bin/python3
+
 import configparser
 import click
 import cachetclient
 from cachetclient.v1 import enums
 import sys
-from jinja2 import Template
+import os
 
 @click.group()
 @click.option('--debug/--no-debug', default=False, help="Enable verbose output")
@@ -16,9 +18,9 @@ def main(debug):
     load_config()
 
 def load_config():
-    # Load config file, raise exception if file not found
+    # Load config file
     config = configparser.ConfigParser()
-    config.read('config.ini')
+    config.read(os.path.join(sys.path[0],'config.ini'))
     # Read CACHET section, raise error if not found
     if not 'CACHET' in config.sections():
         click.secho("[!] Configuration file config.ini is missing or invalid")
@@ -44,17 +46,17 @@ def load_config():
 @click.argument('component_id')
 @click.argument('severity')
 @click.argument('subcomponent_name')
-@click.argument('subcomponent_group')
-def create(component_id, severity, subcomponent_name, subcomponent_group):
+def create(component_id, severity, subcomponent_name):
     component = CachetComponent(component_id)
-    component.create_incident(severity, subcomponent_name, subcomponent_group)
+    component.create_incident(severity, subcomponent_name)
     
 
 @main.command(help='Update Cachet Incident')
 @click.argument('component_id')
-def ack(component_id):
+@click.argument('update_message')
+def ack(component_id, update_message):
     component = CachetComponent(component_id)
-    component.acknowledge_incident()
+    component.acknowledge_incident(update_message)
 
 
 @main.command(help="Resolve Cachet Incident")
@@ -73,6 +75,7 @@ class CachetComponent(object):
             click.secho("[D] Initializing Cachet Component with ID %s" % component_id, fg='green')
         if not self.ping():
             click.secho("[!] Unable to connect to Cachet server at %s" % endpoint, fg='red')
+        self.latest_incident = self._get_most_recent_incident()
 
     def ping(self):
         if self.client.ping():
@@ -82,53 +85,47 @@ class CachetComponent(object):
         else:
             return False
 
-    def _render_template(self, subcomponent_group, subcomponent_name, component_name, type='create'):
-        create_template = Template('''
-Our team is aware of an issue affecting one of our **{{ subcomponent_group }}**`{{ subcomponent_name }}` in **{{ component_name }}**. 
-We are currently investigating the cause of the issue.
-
-We will have an update as soon as more information is available.
-        ''')
-
-        if type=='create':
-            return create_template.render(subcomponent_group=subcomponent_group,subcomponent_name=subcomponent_name,component_name=component_name)
-
-    def _get_active_incidents_for_component(self):
-        return [incident for incident in self.client.incidents.list() if incident.component_id == int(self.component_id) and incident.status != 4]
-
-    def _filter_most_recent_incident(self, incidents_list):
+    def _get_most_recent_incident(self):
         listdates = []
-        for incident in incidents_list:
-            listdates.append(incident.created_at)
-        latest_incident_time = max(listdates)
-        latest_incident= [incident for incident in incidents_list if incident.created_at == latest_incident_time]
-        return latest_incident
+        try: 
+            incidents_list = [incident for incident in self.client.incidents.list() if incident.component_id == int(self.component_id) and incident.status != 4]
+            for incident in incidents_list:
+                listdates.append(incident.created_at)
+            latest_incident_time = max(listdates)
+            latest_incident= [incident for incident in incidents_list if incident.created_at == latest_incident_time]
+            return latest_incident
+        except:
+            return []
 
-    def create_incident(self, severity, subcomponent_group, subcomponent_name):
+    def create_incident(self, severity, subcomponent_name):
+        # get component group
+        component_group_id = self.client.components.get(component_id=self.component_id).group_id
+        component_group_name = self.client.component_groups.get(group_id=component_group_id).name
         self.client.incidents.create(
-            # name="Something blew up! on %s" % subcomponent_name,
-            # message="We are looking into it",
             status=enums.INCIDENT_INVESTIGATING,
             component_id=self.component_id,
             component_status=severity,
-            name = 'Incident affecting %s %s' % (subcomponent_group, subcomponent_name),
-            message = self._render_template(subcomponent_group, subcomponent_name, self.client.components.get(component_id=self.component_id).description, type='create')
+            name = 'Incident affecting %s %s' % (component_group_name, subcomponent_name),
+            message = '''
+Our team is aware of an issue affecting **%s** component (`%s`) in **%s**. 
+We are currently investigating the cause of the issue.
+
+The status of the incident will be updated as soon as more information is available.
+            ''' % (component_group_name, subcomponent_name, self.client.components.get(component_id=self.component_id).description)
         )
 
-    def acknowledge_incident(self):
-        component_incidents = self._get_active_incidents_for_component()
-        latest_incident = self._filter_most_recent_incident(component_incidents)
+    def acknowledge_incident(self, update_message=''):
+        if not len(update_message) > 0:
+            update_message = "We have identified the issue and we are currently working on a fix"
         self.client.incident_updates.create(
-            incident_id = latest_incident[0].id,
+            incident_id = self.latest_incident[0].id,
             status = 2,
-            message = "We have identified the issue and we are currently working on a fix"
+            message = update_message
         )
     
     def resolve_incident(self):
-        component_incidents = self._get_active_incidents_for_component()
-        latest_incident = self._filter_most_recent_incident(component_incidents)
         self.client.incident_updates.create(
-            incident_id = latest_incident[0].id,
+            incident_id = self.latest_incident[0].id,
             status = 4,
             message = "The issue has been resolved"
         )
